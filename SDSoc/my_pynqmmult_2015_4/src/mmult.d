bd5de5e4901bbbfefabc4e3182adcc59598ddced
ryutakashino <s1611354@u.tsukuba.ac.jp>
2017-03-29 19:23:04 +0900

    add some directory

---
+++ SDSoc/my_pynqmmult_2015_4/src/mmult.d
@@ -0,0 +1,8 @@
+C:/Users/ryuta/Documents/SDSoc/test_pynq_mmult/SDRelease/_sds/swstubs/mmult.o: \
+ C:/Users/ryuta/Documents/SDSoc/test_pynq_mmult/SDRelease/_sds/swstubs/mmult.cpp \
+ C:/Users/ryuta/Documents/SDSoc/test_pynq_mmult/src/mmult_accel.h \
+ C:/Xilinx/SDSoC/2015.4/arm-xilinx-linux-gnueabi/include/sds_lib.h
+
+C:/Users/ryuta/Documents/SDSoc/test_pynq_mmult/src/mmult_accel.h:
+
+C:/Xilinx/SDSoC/2015.4/arm-xilinx-linux-gnueabi/include/sds_lib.h:
---
+++ SDSoc/my_pynqmmult_2015_4/src/mmult_accel.d
@@ -0,0 +1,19 @@
+mmult_accel.o: mmult_accel.cpp \
+ C:/Users/ryuta/Documents/SDSoc/test_pynq_mmult/src/mmult_accel.h \
+ cf_stub.h \
+ C:/Xilinx/SDSoC/2015.4/arm-xilinx-linux-gnueabi/include/sds_incl.h \
+ portinfo.h \
+ C:/Xilinx/SDSoC/2015.4/arm-xilinx-linux-gnueabi/include/accel_info.h \
+ C:/Xilinx/SDSoC/2015.4/arm-xilinx-linux-gnueabi/include/sds_lib.h
+
+C:/Users/ryuta/Documents/SDSoc/test_pynq_mmult/src/mmult_accel.h:
+
+cf_stub.h:
+
+C:/Xilinx/SDSoC/2015.4/arm-xilinx-linux-gnueabi/include/sds_incl.h:
+
+portinfo.h:
+
+C:/Xilinx/SDSoC/2015.4/arm-xilinx-linux-gnueabi/include/accel_info.h:
+
+C:/Xilinx/SDSoC/2015.4/arm-xilinx-linux-gnueabi/include/sds_lib.h:
